952bac0948237b4589a8aebaa3d8c606df73c3a1
sagar <sagartubearn@gmail.com>
2019-11-17 10:21:40 +0530

    InitialCommit

---
+++ FilterFormEditorControls/FilterFormEditorControls/Form1.Designer.vb
@@ -0,0 +1,65 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Frm1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.fg = New C1.Win.C1FlexGrid.C1FlexGrid()
+        Me.C1Button1 = New C1.Win.C1Input.C1Button()
+        CType(Me.fg, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.C1Button1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'fg
+        '
+        Me.fg.ColumnInfo = "10,1,0,0,0,-1,Columns:"
+        Me.fg.Location = New System.Drawing.Point(0, 0)
+        Me.fg.Name = "fg"
+        Me.fg.Size = New System.Drawing.Size(500, 150)
+        Me.fg.TabIndex = 0
+        '
+        'C1Button1
+        '
+        Me.C1Button1.Location = New System.Drawing.Point(424, 354)
+        Me.C1Button1.Name = "C1Button1"
+        Me.C1Button1.Size = New System.Drawing.Size(75, 23)
+        Me.C1Button1.TabIndex = 1
+        Me.C1Button1.Text = "C1Button1"
+        Me.C1Button1.UseVisualStyleBackColor = True
+        '
+        'Frm1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.C1Button1)
+        Me.Controls.Add(Me.fg)
+        Me.Name = "Frm1"
+        Me.Text = "Form1"
+        CType(Me.fg, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.C1Button1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents fg As C1.Win.C1FlexGrid.C1FlexGrid
+    Friend WithEvents C1Button1 As C1.Win.C1Input.C1Button
+End Class
---
+++ FilterFormEditorControls/FilterFormEditorControls/Form1.vb
@@ -0,0 +1,57 @@
+﻿Imports System.Xml
+Imports C1.Win.C1FlexGrid
+Imports Castle.MicroKernel.SubSystems.Conversion
+
+Public Class Frm1
+
+    Dim FilterEditorForm As New Form()
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        fg.AllowFiltering = True
+        fg.AllowSorting = AllowSortingEnum.None
+
+        Dim dtColsAndRows As DataTable = New DataTable()
+
+        dtColsAndRows.Columns.Add("Id")
+        dtColsAndRows.Columns.Add("Name")
+        dtColsAndRows.Columns.Add("Location")
+
+        dtColsAndRows.Rows.Add(1, "SPI", "Mysuru")
+        dtColsAndRows.Rows.Add(2, "SoftVision", "Bengaluru")
+        dtColsAndRows.Rows.Add(3, "Cognizant", "USA")
+
+        fg.DataSource = dtColsAndRows
+
+    End Sub
+
+    Private Sub fg_MouseClick(sender As Object, e As MouseEventArgs) Handles fg.MouseClick
+        If fg.HitTest(e.Location).Type = HitTestTypeEnum.FilterIcon Then
+            For Each _frm As Form In Application.OpenForms
+                If _frm.Name = "FilterEditorForm" And _frm.GetType.ToString() = "C1.Win.C1FlexGrid.FilterEditorForm" Then
+                    FilterEditorForm = _frm
+                    Dim X As String = fg.FilterDefinition.Replace(Chr(34), ChrW(&H27))
+                    Dim num As Integer
+                    Dim flag As Boolean
+
+                    For num = 0 To fg.Rows.Count - 1
+                        flag = fg.Rows(num).Selected
+                    Next
+
+                End If
+            Next
+        End If
+    End Sub
+
+    Private Sub C1Button1_Click(sender As Object, e As EventArgs) Handles C1Button1.Click
+        Dim X As String = "<ColumnFilters><ColumnFilter ColumnIndex='0' ColumnName='Id' DataType='System.String'>
+<ValueFilter><ShowValues><Value Value='1' /><Value Value='3' /></ShowValues></ValueFilter>
+</ColumnFilter><ColumnFilter ColumnIndex='1' ColumnName='Name' DataType='System.String'>
+<ValueFilter><ShowValues>
+<Value Value='Cognizant' /></ShowValues></ValueFilter></ColumnFilter></ColumnFilters>"
+
+
+
+        fg.FilterDefinition = Nothing
+        fg.FilterDefinition = X
+    End Sub
+End Class
